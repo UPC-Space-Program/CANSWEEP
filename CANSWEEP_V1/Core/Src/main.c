0909cd23dd7d7bb8a86bfe131da73fb1e2a10608
pauserrat <pauserrat03@gmail.com>
2025-08-03 18:56:03 +0200

    spi bits changed
    the previous datasize of the spi was 4bits which was incorrect. the current spi protocol sends 2 bytes (8 bits each) separately.

--- CANSWEEP_V1/Core/Src/main.c
+++ CANSWEEP_V1/Core/Src/main.c
@@ -85,7 +85,6 @@ static void MX_TIM1_Init(void);
 static void MX_TIM2_Init(void);
 static void MX_TIM5_Init(void);
 static void MX_TIM8_Init(void);
-
 /* USER CODE BEGIN PFP */
 
 // Funciones de control del driver DRV8711
@@ -659,7 +658,7 @@ static void MX_SPI1_Init(void)
   hspi1.Instance = SPI1;
   hspi1.Init.Mode = SPI_MODE_MASTER;
   hspi1.Init.Direction = SPI_DIRECTION_2LINES;
-  hspi1.Init.DataSize = SPI_DATASIZE_4BIT;
+  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
   hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
   hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
   hspi1.Init.NSS = SPI_NSS_HARD_INPUT;
